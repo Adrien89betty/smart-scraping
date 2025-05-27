7dcde7eb9fe8e65e9fcbb081abc16eea138811d1
adrien leveille <adrienleveille.contact@gmail.com>
2025-05-27 10:36:03 +0200

    Modifying middlewares classes names

--- smart_scraper/smart_scraper/middlewares.py
+++ smart_scraper/smart_scraper/middlewares.py
@@ -10,7 +10,7 @@ from scrapy import signals
 from itemadapter import is_item, ItemAdapter
 
 
-class IdsideScraperSpiderMiddleware:
+class SmartScraperSpiderMiddleware:
     # Not all methods need to be defined. If a method is not defined,
     # scrapy acts as if the spider middleware does not modify the
     # passed objects.
@@ -57,7 +57,7 @@ class IdsideScraperSpiderMiddleware:
         spider.logger.info("Spider opened: %s" % spider.name)
 
 
-class IdsideScraperDownloaderMiddleware:
+class SmartScraperDownloaderMiddleware:
     # Not all methods need to be defined. If a method is not defined,
     # scrapy acts as if the downloader middleware does not modify the
     # passed objects.
